2169f37fd49ea9e66e37762f9b7c69e2b76f3bbf
Chirag Mittal <chirag.workid@gmail.com>
2021-09-15 13:17:34 +0530

    array user input

--- src/com/company/strings.java
+++ src/com/company/strings.java
@@ -1,9 +1,20 @@
 package com.company;
 
 import java.util.Locale;
-
+import java.util.Scanner;
 public class strings {
     public static void main(String args[]){
+        int [] arr;
+        arr = new int [3];
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<3;i++){
+            System.out.print("Enter the number : ");
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<3;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println("\n");
         String x="Hello";
         System.out.println(x);
         System.out.println(x.length());
