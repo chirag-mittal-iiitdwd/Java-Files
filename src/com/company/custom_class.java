8ea8d40bd99a90f1697678baa00f1b19e62224fd
Chirag Mittal <chirag.workid@gmail.com>
2021-09-16 17:29:49 +0530

    Things added

--- src/com/company/custom_class.java
+++ src/com/company/custom_class.java
@@ -7,6 +7,15 @@ class employee{
 }
 public class custom_class {
     public static void main(String[] args) {
+        System.out.println("This is a custom class");
 
+        // Instantiating a new Employee Object
+        employee chirag =new employee();
+        System.out.println(chirag.id+" "+chirag.name);
+
+        // setting Properties
+        chirag.id=12;
+        chirag.name="Chirag";
+        System.out.println(chirag.id+" "+chirag.name);
     }
 }
